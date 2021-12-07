f82f8093a431744354efa537ab0473f8a3d108f9
kavyanagendra24 <kavyanagendra24@gmail.com>
2021-12-07 12:51:23 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-vhp6dp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-vhp6dp)
---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+public num1:number;
+public num2:number;
+public num3:number;
+add(){
+  this.num3 = this.num1 + this.num2;
+}
+sub(){
+  this.num3 = this.num1 - this.num2;
+}
+multiply(){
+  this.num3 = this.num1 * this.num2;
+}
+div(){
+  this.num3 = this.num1 / this.num2;
+}
+
+
+
+
+
+}
